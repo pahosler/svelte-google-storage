4553eaffa162c4ba67fd591ffd9689a2f4ffabf1
pahosler <pahosler@gmail.com>
2021-01-30 23:34:52 -0600

    more stuff

---
+++ api/storage.js
@@ -0,0 +1,38 @@
+import multer, {memoryStorage} from "multer";
+import storage from "@google-cloud/storage";
+import crypto from "crypto";
+import {encrypted} from "./service-account.enc";
+
+// multer - create memory storage for image uploaded via req.files
+const m = multer({
+    storage: memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024 // no larger than 5mb
+    }
+});
+
+// decrypt keyfile
+const algorithm = 'aes-128-cbc';
+const decipher = crypto.createDecipheriv(
+    algorithm,
+    process.env.GOOGLE_ENGRYPTION_KEY,
+    process.env.GOOGLE_ENCRYPTION_IV
+);
+
+const getDecryptedSecret = () => {
+    let decrypted = decipher.update(encrypted, 'base64', 'utf8');
+
+    decrypted += decipher.final('utf8');
+
+    return JSON.parse(decrypted);
+}
+
+//Instantiate storage client
+const {project_id, client_email, private_key} = getDecryptedSecret();
+const GCS = storage({
+    projectId: project_id,
+    credentials: {
+        client_email,
+        private_key
+    }
+})

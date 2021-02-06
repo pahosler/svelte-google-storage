02856e89c98b582a9d7bc1493f9c2335ff80607e
pahosler <pahosler@gmail.com>
2021-02-05 18:53:29 -0600

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -47,16 +47,6 @@
   };
 
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const data = await res.json();
-    const thing = await fetch("/api/name", { method: 'POST', body: "Fred" })
-    const thingd = await thing.json()
-    console.log(thingd)
-    date = data.date;
-    bucket_name = data.bucket_name;
-  });
-
 </script>
 
 <main>
@@ -68,31 +58,9 @@
     </a>
     !
   </h2>
-  <p>
-    <a href="https://github.com/vercel/vercel/tree/master/examples/svelte" target="_blank" rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
+
   <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
-  <p>{bucket_name ? bucket_name : 'nope'}</p>
+
   <input type="button" on:click={login} value="LOGIN" disabled>
   <form action="/api/createBucket">
     <input type="submit" value="Create Bucket!" disabled>
